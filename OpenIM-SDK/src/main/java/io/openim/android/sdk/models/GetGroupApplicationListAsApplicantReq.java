374c9988c6a46422967e6acbe3c0635daaa81e47
Johnny <mac@Johnny-Mac-mini.local>
2026-01-29 10:15:26 +0800

    feat: support pagination parameters

--- OpenIM-SDK/src/main/java/io/openim/android/sdk/models/GetGroupApplicationListAsApplicantReq.java
+++ OpenIM-SDK/src/main/java/io/openim/android/sdk/models/GetGroupApplicationListAsApplicantReq.java
@@ -1,8 +1,41 @@
 package io.openim.android.sdk.models;
 
 public class GetGroupApplicationListAsApplicantReq {
+    private int offset;
+    private int count;
     private String[] groupIDs;
     private int[] handleResults;
+
+    public GetGroupApplicationListAsApplicantReq() {
+    }
+
+    public GetGroupApplicationListAsApplicantReq(int offset, int count) {
+        this.offset = offset;
+        this.count = count;
+    }
+
+    public GetGroupApplicationListAsApplicantReq(int offset, int count, String[] groupIDs, int[] handleResults) {
+        this.offset = offset;
+        this.count = count;
+        this.groupIDs = groupIDs;
+        this.handleResults = handleResults;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 
     public String[] getGroupIDs() {
         return groupIDs;
